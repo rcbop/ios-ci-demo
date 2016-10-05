2e8e3ef32655da756151f4a459c1e3f8a4b6ceff
Rogério Peixoto <rogerio.c.peixoto@accenture.com>
2016-10-05 20:09:13 -0300

    new test stuff

--- MyCIDemoTests/MyCIDemoTests.swift
+++ MyCIDemoTests/MyCIDemoTests.swift
@@ -24,12 +24,14 @@ class MyCIDemoTests: XCTestCase {
     func testExample() {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct results.
+        XCTAssert(true, "Pass")
     }
     
     func testPerformanceExample() {
         // This is an example of a performance test case.
         self.measureBlock {
             // Put the code you want to measure the time of here.
+            XCTAssert(true, "Pass")
         }
     }
     
