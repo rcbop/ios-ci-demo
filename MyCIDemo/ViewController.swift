582911e6cae4f9540f1b586e8cdb3a0986a77f6c
Rogério Peixoto <rogerio.c.peixoto@accenture.com>
2016-10-05 17:06:46 -0300

    initial commit

---
+++ MyCIDemo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MyCIDemo
+//
+//  Created by Rogerio Peixoto on 10/5/16.
+//  Copyright © 2016 TestInc. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
